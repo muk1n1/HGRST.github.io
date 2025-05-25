3cd62f1aa90d734435117d029a341c027056b885
muk1n1 <vlad.nesterenko.07@inbox.ru>
2025-05-25 12:27:03 +0300

    Add files via upload

---
+++ HTML 1/qasw/zxc.js
@@ -0,0 +1,21 @@
+// Простейший калькулятор
+function calculate(operation, num1, num2) {
+    switch(operation) {
+        case 'add':
+            return num1 + num2;
+        case 'subtract':
+            return num1 - num2;
+        case 'multiply':
+            return num1 * num2;
+        case 'divide':
+            return num1 / num2;
+        default:
+            return 'Invalid operation';
+    }
+}
+
+// Примеры использования калькулятора
+console.log(calculate('add', 5, 3));       // Результат: 8
+console.log(calculate('subtract', 5, 3));  // Результат: 2
+console.log(calculate('multiply', 5, 3));  // Результат: 15
+console.log(calculate('divide', 6, 3));    // Результат: 2
